a0e7abb54a031d22603b2f6344b2330a168a366d
jignesh1001 <jigneshgurav11@gmail.com>
2025-12-29 00:15:49 +0530

    changed : works negative integers also

--- MaxSubarraySumK.java
+++ MaxSubarraySumK.java
@@ -3,20 +3,19 @@ class MaxSubarraySumK{
         int[] arr = {-2,-3,4,-1,-2,1,5,-3};
         maxSubarraySumk(arr);
     }
-    public static maxSubarraySumk(int[] arr){
-         // does not work for case where all elements
-         // are negative
+    public static int maxSubarraySumk(int[] arr){
+        // works for all cases 
          int maxSum = Integer.MIN_VALUE;
          int sum = 0;
 
          for(int i = 0;i < arr.length;i++){
             sum += arr[i];
+            maxSum = Math.max(sum,maxSum);
             if(sum < 0){
                 sum = 0;
             }
-            maxSum = Math.max(sum,maxSum);
          }
          System.out.println(maxSum);
-
+        return maxSum;
     }
 }
